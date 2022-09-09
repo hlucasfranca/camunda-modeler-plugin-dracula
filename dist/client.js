dfd454c97d30c7ecdac7d851b48dcde69036d8e3
Henrique Lucas França <hlucasfranca@gmail.com>
2022-09-09 00:07:57 -0300

    fix: rendering compatibility

--- dist/client.js
+++ dist/client.js
@@ -101,25 +101,12 @@ function DraculaTheme(eventBus) {
   function changeColors(event) {
 
     const gfx = event.gfx;
+    const element = event.element;
 
-    if(gfx.children.length > 0 && gfx.children[0].children){
-      for (let child of gfx.children[0].children) {
-
-        let style = child.style;
-        let fill = style.fill;
-
-        if( style.fill == 'white' || style.fill == "rgb(255, 255, 255)"){
-          style.fill = '#262936';  
-        } else if (style.fill == 'black' || style.fill == 'rgb(0, 0, 0)' || style.fill == 'rgb(34, 36, 42)'|| style.fill == 'rgb(38, 41, 54)'){
-          style.fill = 'rgb(207, 205, 205)';
-        }       
-        
-        if(style.stroke == 'black' || style.stroke == 'rgb(0, 0, 0)' || style.stroke == 'rgb(34, 36, 42)'|| style.stroke == 'rgb(38, 41, 54)'){
-          style.stroke = 'rgb(207, 205, 205)';
-        }
-      }
+    if(element){
+      element.di['background-color']='#262936';
+      element.di['border-color']='rgb(207, 205, 205)';
     }
-    
   }
 
 eventBus.on([
@@ -127,8 +114,9 @@ eventBus.on([
   'render.shape', 
   'render.connection',
   'shape.moved',
-  'shape.changed'
-], changeColors);
+  'shape.changed',
+  'element.changed'
+], 1250, changeColors);
 
 }
 
